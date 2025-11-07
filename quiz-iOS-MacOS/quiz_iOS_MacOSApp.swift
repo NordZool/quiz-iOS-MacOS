e2d7b6938dacdbb2e4083e08a05e6b9e75ab0aa0
NordZool <nordzooldev@gmail.com>
2025-11-07 12:58:03 +0300

    Initial Commit

---
+++ quiz-iOS-MacOS/quiz_iOS_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  quiz_iOS_MacOSApp.swift
+//  quiz-iOS-MacOS
+//
+//  Created by Никита on 7.11.25.
+//
+
+import SwiftUI
+
+@main
+struct quiz_iOS_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
